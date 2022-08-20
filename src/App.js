cee72feb9ffa4111a0f5bf3399f556fd25f44960
spde3289 <spde3289@naver.com>
2022-08-20 12:57:28 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/header'
+
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <div className="top">
         <div className="box">asd</div>
         <div className="box">asd</div>
