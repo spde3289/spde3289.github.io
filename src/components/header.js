19427a39eb1145209ae603f305fbd121e24aff7a
spde3289 <spde3289@naver.com>
2022-08-21 19:53:43 +0900

    add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,20 @@
+import './header.css'
 
 const Header = () => {
     return(
         <header>
-            header
+            <div><a href='https://spde3289.github.io/'>spde3289.github.io</a></div>
+            <div>
+                <img src={require('../imgs/github.png')} alt='github Link'/>
+            </div>
+            <ul>
+                <li>as</li>
+                <li><a href='/'>asd</a></li>
+            </ul>
+            <ol>
+                <li>a</li>
+                <li>a</li>
+            </ol>
         </header>
     );
 };
