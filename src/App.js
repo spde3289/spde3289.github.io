0155083303b1595a0e4363fb36b27d70bbb3db78
spde3289 <spde3289@naver.com>
2022-08-27 20:46:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header'
+import Body from './components/body'
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Body></Body>
     </div>
   );
 }
