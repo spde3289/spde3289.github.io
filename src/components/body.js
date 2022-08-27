a7ef7cf1733a90d43838d82673accb80a8a2a38a
spde3289 <spde3289@naver.com>
2022-08-27 20:46:11 +0900

    Create body.js

---
+++ src/components/body.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Body = () => {
+    return(
+        <>
+        <body>
+        <Box> asd </Box>
+        </body>
+        </>
+    );
+};
+
+const Box = styled.div`
+    width: 100px;
+    height: 100px;
+    font-size: 40px;
+    font-weight: bold;
+`;
+
+
+
+export default Body;
