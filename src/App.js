6e0db47eadea68e76f6db85bd196c348b2097063
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-08-30 22:26:30 +0900

    s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './pages/About';
 
 function App() {
   return (
- 
+    <BrowserRouter>
       <Routes><Route  Path='Home' element={<Home/>} /></Routes>
       <Routes><Route Path='About' element={<About/>} /> </Routes>
     </BrowserRouter>
