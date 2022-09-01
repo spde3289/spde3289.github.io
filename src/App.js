15a67646d89756ff98d89a5746de866b018a12f7
spde3289 <spde3289@naver.com>
2022-09-01 23:47:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
-import About from './pages/About';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes><Route  Path='Home' element={<Home/>} /></Routes>
-      <Routes><Route Path='About' element={<About/>} /> </Routes>
-    </BrowserRouter>
+    <div>
+      <Home></Home>
+    </div>
   );
 };
 
