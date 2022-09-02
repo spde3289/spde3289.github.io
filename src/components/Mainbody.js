fd2d7a9377023ccba1ad0b3573d5236f648f52f2
spde3289 <spde3289@naver.com>
2022-09-02 21:12:58 +0900

    Update Mainbody.js

--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -1,11 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import {Link} from 'react-router-dom';
-
 
 const MainBody = () => {
     return(
-          <Box> asd  <Link to = "/about">about</Link> </Box>
+          <Box> asd </Box>
     );
 };
 
