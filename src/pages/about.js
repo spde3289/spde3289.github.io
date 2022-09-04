e1dd496eaf337b70ac9cbc7fdbc4298af1b54993
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-09-04 22:46:15 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import MainHeader from '../components/MainHeader'
 
 const About = () => {
     return(
-        <div>About</div>
+        <div>
+        <MainHeader></MainHeader>
+        <div>
+            개발블로그를 시작한 김지훈 이라고 합니다.<p/>
+            이곳에 저의 성장과정과 근황을 올리려고 합니다. 
+        </div>
+        <div><a href='https://github.com/spde3289'>깃헙</a></div>
+        </div>
     );
 }
 
