193fb72ddc49c266c6d2b59a3be172fd59df0a38
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-09-06 05:35:47 +0900

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,13 @@ import styled from "styled-components";
 const Footer = () => {
     return(
         <FooterBar>
-            <div>Copyright © 2022, All right reserved.</div>
+            <p><a href='https://github.com/spde3289'>깃허브 아이콘</a></p>
+            <p>myer100756@gmail.com, spde3289@naver.com</p>
         </FooterBar>
     );
 };
 
 const FooterBar = styled.footer`
-    position: relative;
-    
     padding: 40px 20px ;
     text-align: center;
 `
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from "styled-components";
+/* import styled from "styled-components"; */
 import MainHeader from '../components/Header'
 import Footer from '../components/Footer'
 
@@ -11,17 +11,12 @@ const About = () => {
             개발블로그를 시작한 김지훈 이라고 합니다.<p/>
             이곳에 저의 성장과정과 근황을 올리려고 합니다. 
         </div>
-        <Box>asd</Box>
-        <div><a href='https://github.com/spde3289'>깃헙</a></div>
+        <div>사용한 기술 : React, styled-components, router, </div>
         <Footer></Footer>
         </div>
     
     );
 };
 
-const Box = styled.div`
-align-items: center;
-width: 700px;
-`
 
 export default About;
