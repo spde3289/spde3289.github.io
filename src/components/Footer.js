9bfc5fc790288dc081eab6ee2ebb9dd7f3ea183a
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-09-07 17:41:30 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,41 @@
 import styled from "styled-components";
-import { AiFillGithub } from 'react-icons/ai'
+import { AiFillGithub, AiOutlineMail } from 'react-icons/ai'
+import { IoLogoInstagram } from 'react-icons/io'
 
 const Footer = () => {
     return(
         <FooterBar>
-            <Footerinfo><a href='https://github.com/spde3289'><AiFillGithub size='24'/></a></Footerinfo>
-            <Footerinfo>myer100756@gmail.com, spde3289@naver.com</Footerinfo>
+            Contact Information
+            <Footerinfo>
+                <a href='mailto:spde3289@gmail.com'>
+                    <AiOutlineMail style={{marginBottom: -6, marginRight: 5}} size='24'/>myer100756@gmail.com
+                </a>
+            </Footerinfo>
+            <Footerinfo>
+                <a href='https://github.com/spde3289'>
+                    <AiFillGithub style={{marginBottom: -6, marginRight: 5}} size='24'/>spde3289
+                </a>
+            </Footerinfo>
+            <Footerinfo>
+                <a href='https://www.instagram.com/98._.hun/'>
+                    <IoLogoInstagram style={{marginBottom: -6, marginRight: 5}} size='24'/>98._.hun
+                </a>
+            </Footerinfo>
         </FooterBar>
     );
 };
 
 const FooterBar = styled.footer`
     padding: 40px 20px ;
-`
+    text-align: center;
+`;
 
 const Footerinfo = styled.p`
     font-size: 15px;
     margin-right: 10px;
 `
+
+
+
 
 export default Footer;
