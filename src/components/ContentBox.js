121c048611b310e2b6d038cb025a6eb6acb46df5
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-09-10 21:45:21 +0900

    update main

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -2,18 +2,33 @@ import React from 'react';
 import styled from "styled-components";
 
 const ContentBox = (props) => {
-    console.log(props.title)
     return(
-        <Content>{props.title}</Content>
+        <PostBox>
+            <PostBoxTitle>
+                {props.title}
+            </PostBoxTitle>
+            <PostBoxContent>
+                {props.content}
+            </PostBoxContent>
+        </PostBox>
     );
 };
 
-const Content = styled.div`
+const PostBox = styled.div`
     width: 600px;
     height: 120px;
     margin: 10px auto;
     border: .3px solid #000;
     border-radius: 10px;
+`;
+
+const PostBoxTitle = styled.h2`
+    margin: 10px;
+`;
+
+const PostBoxContent = styled.p`
+    margin: 15px;
+    font-size: 13px;
 `;
 
 export default ContentBox;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,15 +14,18 @@ const Home = () => {
                 안녕하세요. <p/>
                 꾸준하게 공부하는 주니어 개발자 김지훈입니다.
                 </Introduction>
+                <Content>
                 <ContentBoxArea>
                     최신 포스트 
-                    <ContentBox title="나의 첫 글" ></ContentBox>
+                    <ContentBox title="나의 첫 글" content='나의 첫 글 내용'></ContentBox>
                     <ContentBox title="나의 두번째 글"></ContentBox>
                     <ContentBox title="나의 두번째 글"></ContentBox>
                     <ContentBox title="나의 두번째 글"></ContentBox>
                     <ContentBox title="나의 두번째 글"></ContentBox>
+                    <ContentsMore>more</ContentsMore>
                 </ContentBoxArea>
-
+                <ConstentList></ConstentList>
+                </Content>
             </Main>
             <Footer></Footer>
         </div>
@@ -39,7 +42,13 @@ const Main = styled.main`
 
 const Introduction = styled.div`
     font-size: 20px;
-`
+`;
+
+const Content = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
+
 
 const ContentBoxArea = styled.div`
     width: 640px;
@@ -49,7 +58,23 @@ const ContentBoxArea = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-`
+`;
+
+const ContentsMore = styled.button`
+    margin: 0 auto;
+    width: 100px;
+    height: 30px;
+    border: 1px solid #bbb;
+    border-radius: 10px;
+    text-align: center;
+`;
+
+const ConstentList = styled.div`
+    border: 1px solid #000;
+    width: 270px;
+`;
+
+
 
 export default Home;
 
