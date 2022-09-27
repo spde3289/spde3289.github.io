ab707d1a75a9691bc09c1cde06ee3d452fc80441
spde3289 <spde3289@naver.com>
2022-09-27 21:13:22 +0900

    Update First.js

--- src/pages/post/First.js
+++ src/pages/post/First.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import styled from "styled-components";
-import ReactMarkdown from 'react-markdown';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-
-const MarkDownstyle = styled.div`
-    width: 1000px;
-    margin: 0 auto;
-    font-size: 1rem;
-    line-height: 2.5rem;
-    `;
-
-const markdown = `
-# useEffect
-첫 글은 내가 블로그를 만들면서 있었던 문제들을 적어보려 한다.
-처음 렌더할때 데이터를 받아와 처리해야하는 일이 있는데 
-useEffect를 
-
-`;
-
-
 
 const FirstPost = () => {
     return(
         <>
-        <Header/>
-        <MarkDownstyle>
-            <ReactMarkdown>
-                {markdown}
-            </ReactMarkdown>
-        </MarkDownstyle>
-        <Footer/>
+            <Header/>
+            <Main>
+                <ContentHeader>
+                <h1>useEffect 무한루프</h1>
+                <Info>
+                    
+                </Info>
+                </ContentHeader>
+            </Main>
+            <Footer/>
         </>
     );
 };
+    
+const Main = styled.div`
+    width: 1000px;
+    margin: 0 auto;
+    font-size: 1rem;
+    line-height: 2.5rem;
+`;
+
+const ContentHeader = styled.header`
+    
+`;
+
+const Info = styled.div`
+
+`
 
 export default FirstPost;
