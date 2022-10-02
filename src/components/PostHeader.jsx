fa478d7a873078bc4343f27f82da06731264aa28
spde3289 <spde3289@naver.com>
2022-10-02 16:50:01 +0900

    Create PostHeader.jsx

---
+++ src/components/PostHeader.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "styled-components";
+import Tag from './Tag';
+
+const PostHeader = (props) => {
+    return(
+        <>
+            <ContentHeader>
+                <Tag tagName={props.tagName}/>
+                <Title>{props.title}</Title>
+                <Info>
+                    {props.date}
+                </Info>
+            </ContentHeader>
+        </>
+    );
+};
+
+const ContentHeader = styled.header`
+    margin-bottom: 50px;
+    padding: 15px;
+    border-bottom: 1px solid #ccc;
+`;
+
+const Title = styled.h1`
+    font-size: 36px;
+    margin: 20px 0;
+`;
+
+const Info = styled.div`
+    font-size: 15px;
+`;
+
+export default PostHeader;
