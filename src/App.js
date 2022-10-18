fb57dbb8a03bedf35e6d4fcbfd14e50f1ca76277
spde3289 <spde3289@naver.com>
2022-10-18 21:04:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Home from './pages/Home';
 
-function App() {
+function App(props) {
   return (
     <div>
-      <Home></Home>
+      <Home content={props.content}></Home>
     </div>
   );
 };
