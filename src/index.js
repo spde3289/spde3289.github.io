217521cd9774250c7987f1ea54dbe0e554603697
spde3289 <spde3289@naver.com>
2022-10-24 21:12:42 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './css/reset.css'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
