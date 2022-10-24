5ae36fefa2faee7a2015605457e4728a6518fc7d
spde3289 <spde3289@naver.com>
2022-10-24 21:12:46 +0900

    Create postInfo.js

---
+++ src/postInfo.js
@@ -0,0 +1,19 @@
+const content = [
+    {
+        key: 1, 
+        title: 'useEffect 무한루프', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: 'react', 
+        link: '/posts/first'
+    },
+    {
+        key: 2, 
+        title: '첫 글', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: '웹', 
+        link: '/posts/second'},
+];
+
+export default content
