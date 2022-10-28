381a57d257832cb68be9a98672b31afbf5ca6f2e
spde3289 <spde3289@naver.com>
2022-10-28 20:41:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from'./pages/About';
 import Posts from'./pages/Posts';
+import Search from './pages/Search';
 import First from './pages/post/First';
 import Second from './pages/post/Second';
 
@@ -12,6 +13,7 @@ function App(props) {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/posts' element={<Posts/>}/>
+      <Route path='/search' element={<Search/>}/>
       <Route path='/posts/first' element={<First/>}/>
       <Route path='/posts/second' element={<Second/>}/>
     </Routes>
