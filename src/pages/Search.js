5a6fea184b13a7853df33344e8fbc49bd017d8d6
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-10-31 07:29:28 +0900

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Passengers from '../components/Passengers'
 
-const Search = (props) => {
-    console.log(props.value)
+const Search = () => {
+    const location = useLocation();
+
+    const value = location.state.value;
+    console.log(value);
     return(
         <>
             <Header/>
-            <Passengers value={props.value} />
+            
+            <Passengers/>
             <Footer/>
         </>
     );
