93822052ddb90f06ebfcae81011be05c01885594
spde3289 <spde3289@naver.com>
2022-10-31 20:54:35 +0900

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 import { useLocation } from 'react-router-dom';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -12,11 +13,23 @@ const Search = () => {
     return(
         <>
             <Header/>
-            
-            <Passengers/>
+            <Main>
+                <div>{value}</div>
+                <Passengers/>
+            </Main>
             <Footer/>
         </>
     );
-}
+};
+
+const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 1000px;
+    margin: 0 auto;
+`;
+
+
 
 export default Search
