4c0e55c71cf849274b04520429da5cd70c9ed5f8
spde3289 <spde3289@naver.com>
2022-11-16 14:35:37 +0900

    Create Router404.js

---
+++ src/pages/post/Router404.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+import GiscusApp from '../../components/GiscusApp';
+
+const Router404 = () => {
+    return(
+        <>
+        <MarkDownstyle>
+            asdasdasdas
+        </MarkDownstyle>
+            <GiscusApp/>
+        </>
+    );
+};
+
+const MarkDownstyle = styled.div`
+    font-size: 1rem;
+    line-height: 2.5rem;
+`;
+
+
+
+export default Router404;
