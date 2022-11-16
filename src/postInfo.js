e26597deb6e3326dce122cadca3f0fda27bb7c77
spde3289 <spde3289@naver.com>
2022-11-16 14:35:49 +0900

    Update postInfo.js

--- src/postInfo.js
+++ src/postInfo.js
@@ -4,16 +4,16 @@ const content = [
         title: '새로고침시', 
         body: 'asdasd', 
         date:'2022.09.13', 
-        category: '웹', 
-        link: '/posts/second'
+        category: ['웹'], 
+        link: '/posts/router_404'
     },
     {
         key: 1, 
         title: 'useEffect 무한루프', 
         body: 'asdasd', 
         date:'2022.09.13', 
-        category: 'react', 
-        link: '/posts/first'
+        category: ['react', '웹'], 
+        link: '/posts/useEffect_infinite_loop'
     },
 ];
 
