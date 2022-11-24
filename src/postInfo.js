e17692c27178fd914d5bddbd8867aa6f7e91ab80
spde3289 <spde3289@naver.com>
2022-11-24 19:07:10 +0900

    Update postInfo.js

--- src/postInfo.js
+++ src/postInfo.js
@@ -1,18 +1,68 @@
 const content = [
     {
-        key: 2, 
+        key: 1, 
         title: '새로고침시', 
         body: 'asdasd', 
         date:'2022.09.13', 
-        category: ['웹'], 
+        category: [{
+            key: 1, 
+            category: ['웹']
+        }], 
         link: '/posts/router_404'
     },
     {
-        key: 1, 
+        key: 2, 
         title: 'useEffect 무한루프', 
         body: 'asdasd', 
         date:'2022.09.13', 
-        category: ['react', '웹'], 
+        category: [{
+            key: 2, 
+            category: ['웹', 'react']
+        }], 
+        link: '/posts/useEffect_infinite_loop'
+    },
+    {
+        key: 3, 
+        title: 'useEffect 무한루프', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: [{
+            key: 3, 
+            category: ['웹']
+        }], 
+        link: '/posts/useEffect_infinite_loop'
+    },
+    {
+        key: 4, 
+        title: 'useEffect 무한루프', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: [{
+            key: 4, 
+            category: ['웹']
+        }],  
+        link: '/posts/useEffect_infinite_loop'
+    },
+    {
+        key: 5, 
+        title: 'useEffect 무한루프', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: [{
+            key: 5, 
+            category: ['웹']
+        }], 
+        link: '/posts/useEffect_infinite_loop'
+    },
+    {
+        key: 6, 
+        title: 'useEffect 무한루프', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: [{
+            key: 6, 
+            category: ['웹']
+        }], 
         link: '/posts/useEffect_infinite_loop'
     },
 ];
