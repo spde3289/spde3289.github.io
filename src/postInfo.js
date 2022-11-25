55778d14a42033b8d1d39f60730b180073d8b602
spde3289 <47699011+spde3289@users.noreply.github.com>
2022-11-25 22:21:12 +0900

    Update postInfo.js

--- src/postInfo.js
+++ src/postInfo.js
@@ -1,25 +1,25 @@
 const content = [
     {
         key: 1, 
-        title: '새로고침시', 
-        body: 'asdasd', 
-        date:'2022.09.13', 
-        category: [{
-            key: 1, 
-            category: ['웹']
-        }], 
-        link: '/posts/router_404'
-    },
-    {
-        key: 2, 
         title: 'useEffect 무한루프', 
         body: 'asdasd', 
         date:'2022.09.13', 
         category: [{
-            key: 2, 
+            key: 1, 
             category: ['웹', 'react']
         }], 
         link: '/posts/useEffect_infinite_loop'
+    },
+    {
+        key: 2, 
+        title: '새로고침시', 
+        body: 'asdasd', 
+        date:'2022.09.13', 
+        category: [{
+            key: 2, 
+            category: ['웹']
+        }], 
+        link: '/posts/router_404'
     },
     {
         key: 3, 
@@ -41,28 +41,6 @@ const content = [
             key: 4, 
             category: ['웹']
         }],  
-        link: '/posts/useEffect_infinite_loop'
-    },
-    {
-        key: 5, 
-        title: 'useEffect 무한루프', 
-        body: 'asdasd', 
-        date:'2022.09.13', 
-        category: [{
-            key: 5, 
-            category: ['웹']
-        }], 
-        link: '/posts/useEffect_infinite_loop'
-    },
-    {
-        key: 6, 
-        title: 'useEffect 무한루프', 
-        body: 'asdasd', 
-        date:'2022.09.13', 
-        category: [{
-            key: 6, 
-            category: ['웹']
-        }], 
         link: '/posts/useEffect_infinite_loop'
     },
 ];
