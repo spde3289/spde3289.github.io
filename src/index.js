29a4b36a4f1a7ab51969f022411d9df84b076b16
spde3289 <spde3289@naver.com>
2022-11-28 18:49:28 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './css/reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App/>
-  </HashRouter>
+  </BrowserRouter>
 );
