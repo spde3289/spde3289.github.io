05eb13d06bbe9c126ac4eb5416d8b4970761f714
spde3289 <spde3289@naver.com>
2022-12-26 20:29:53 +0900

    Update Router404.js

--- src/pages/post/Router404.js
+++ src/pages/post/Router404.js
@@ -18,8 +18,7 @@ const Router404 = () => {
                         </SubTitle>
                         <PostText>
                             블로그에 댓글 기능을 추가하고 싶어 Giscus을 알아보고 바로 블로그에 적용을 한 후 
-                            잘 동작하나 확인을 하던 와중 갑자기 404 페이지가 떠서 아주 놀랐던 기억이 있다.
-                            react-router를 사용할때 이동페이시에서 새로고침을 했을 때 생기는 문제였다.
+                            잘 동작하나 확인을 하던 와중 갑자기 404NotFound가 떠버리는 상황이다.
                         </PostText>
                         <SubTitle>
                             이유
@@ -27,21 +26,34 @@ const Router404 = () => {
                         <PostText>
                             React 는 CSR(클라이언트 사이트 렌더링)이다. 이를 React-Router 로 Path를
                             이동 시킨다고 생각되지만 이름만 변경될뿐 이동이 되는 것은 아니다. 
-                            최초 접속 이후에 서버에 요청을 하면 index.html 파일을 보내준다 
+                            최초 접속 이후에 서버에 요청을 하면 index.html 파일을 보내주게 되는데 
                         </PostText>
                         <SubTitle>
                             해결방법
                         </SubTitle>
-                        <PostText>
-                            <h3>HashRouter</h3>
-                                일단 HashRouter 를 사용하는 방법이 있다. BrowserRourer를 HashRouter로
-                                변경을 해주면 될 정도로 간단한 방법인데 <br/>단점으로는 못생긴 url과 검색 엔진 최적화(SEO)
-                                가 안 된 다는 것 이다.
-                                <br/>
-                                <br/>
-                            <h3>index.html 리다이렉션 - 깃허브</h3>
-                                이 방법은 이 위에 있는 방법보다는 좀 더 복잡한 방법이다.
-                        </PostText>
+                        <ul>
+                            <li>
+                                <SubHeading>
+                                    HashRouter
+                                </SubHeading>
+                                <PostText>
+                                    일단 HashRouter 를 사용하는 방법이 있다. BrowserRourer를 HashRouter로
+                                    변경을 해주면 될 정도로 간단한 방법인데 단점으로는 못생긴 url과 검색
+                                    엔진 최적화(SEO)가 안 된 다는 것 이다.
+                                </PostText>
+                            </li>
+                            <li>
+                                <SubHeading>
+                                    index.html 리다이렉션 - 깃허브
+                                </SubHeading>
+                                <PostText>
+                                    이 방법은  HashRouter를 사용하기 싫어서 찾은 방법이다. 
+                                </PostText>
+                            </li>
+                        </ul>
+
+                      
+                        
                     </div>
                     <Comment>
                         <B>
@@ -65,12 +77,17 @@ const Main = styled.div`
 
 const SubTitle = styled.h2`
     font-size: 26px;
+    margin: 50px 0 20px;
+`;
+
+const SubHeading =styled.h3`
+    margin: 10px;
 `;
 
 const PostText = styled.p`
+    margin-left: 30px;
     width: 56em;
     font-size: 16px;
-    margin: 50px 0;
 `;
 
 /* const CodeImg = styled.div`
@@ -79,6 +96,7 @@ const PostText = styled.p`
 `; */
 
 const Comment = styled.div`
+    margin: 40px;
     display: flex;
     justify-content: center;
 `;
