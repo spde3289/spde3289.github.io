fbd2b283e6b64d51f5a181ec2c0cdf9da98f1fad
spde3289 <spde3289@naver.com>
2023-02-19 01:35:59 +0900

    feat: remove

--- src/components/GiscusApp.jsx
+++ src/components/GiscusApp.jsx
@@ -3,19 +3,19 @@ import Giscus from '@giscus/react';
 export default function GiscusApp() {
   return (
     <Giscus
+      id='comments'
       repo="spde3289/spde3289.github.io"
       repoId="R_kgDOH_0sSg"
       category="Announcements"
       categoryId="DIC_kwDOF1L2fM4B-hVS"
       mapping="url"
-      term='light'
+      term='Welcome to @giscus/react component!'
       reactionsEnabled="1"
       emitMetadata="0"
-      inputPosition="Welcome to @giscus/react component!"
+      inputPosition="top"
       theme="light"
       lang="ko"
       loading="lazy"
     />
-    
   );
 }
