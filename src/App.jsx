527ec9e9575765814d40a99c08416caad6e43550
spde3289 <47699011+spde3289@users.noreply.github.com>
2023-03-09 06:23:53 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Router404 from './pages/post/Router404';
 import BJ1712 from './pages/post/BJ1712';
 import BJ2292 from './pages/post/BJ2292';
 import BJ4344 from './pages/post/BJ4344';
+import ReactFragment from './pages/post/React-Fragment';
 
 function App() {
 
@@ -53,13 +54,14 @@ function App() {
             <Routes>
               <Route path='/' element={<Home/>}/>
               <Route path='/about' element={<About/>}/>
-              <Route path='/posts' element={<Posts/>}/>
               <Route path='/search' element={<Search/>}/>
+              <Route path='/posts' element={<Posts/>}/>
               <Route path='/posts/UseEffect_infinite_loop' element={<UseEffectInfiniteLoop/>}/>
               <Route path='/posts/Router_404' element={<Router404/>}/>
               <Route path='/posts/BJ1712'  element={<BJ1712/>}/>
               <Route path='/posts/BJ2292' element={<BJ2292/>}/>
               <Route path='/posts/BJ4344' element={<BJ4344/>}/>
+              <Route path='/posts/React-Fragment' element={<ReactFragment/>}/>
             </Routes>
         <Footer/>
         </ThemeProvider>
