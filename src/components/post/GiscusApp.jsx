33341aa67706cc36a4526bd42f17d94b380b4e59
spde3289 <spde3289@naver.com>
2023-04-13 07:59:58 +0900

    fix: giscus 404 error

--- src/components/post/GiscusApp.jsx
+++ src/components/post/GiscusApp.jsx
@@ -1,5 +1,4 @@
 import styled, { useTheme } from 'styled-components';
-import Giscus from '@giscus/react';
 
 export default function GiscusApp() {
 
@@ -7,17 +6,17 @@ export default function GiscusApp() {
 
   return (
     <Container>
-      <Giscus
+      <giscus-widget
         id='comments'
         repo="spde3289/spde3289.github.io"
         repoId="R_kgDOH_0sSg"
-        category="Q&A"
-        categoryId="DIC_kwDOF1L2fM4B-hVS"
-        mapping="title"
+        category="General"
+        categoryId="DIC_kwDOH_0sSs4CR0Uy"
+        mapping="pathname"
         term='Welcome to @giscus/react component!'
         reactionsEnabled="1"
         emitMetadata="0"
-        inputPosition="top"
+        inputPosition="bottom"
         theme={theme.color.comment}
         lang="ko"
         loading="lazy"
